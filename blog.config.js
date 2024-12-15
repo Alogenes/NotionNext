907137d126463d2a4a33d8f483ddf8026d9010aa
Alógenes <51833107+Alogenes@users.noreply.github.com>
2024-12-15 10:41:24 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，soy Jinou, Gracias por tu visitas,Bienvenido a mi blog🎉, Hi，soy Jinou, Gracias por tu visitas,Bienvenido a mi blog🎉',
+   'Este pagina está en actualización, tarda 1 mes de actualización'/* 'Hi，soy Jinou, Gracias por tu visitas,Bienvenido a mi blog🎉, Hi，soy Jinou, Gracias por tu visitas,Bienvenido a mi blog🎉'*/,
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
